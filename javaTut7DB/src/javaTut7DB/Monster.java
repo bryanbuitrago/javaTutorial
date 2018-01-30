254a0a82139bb33e52050459f3d35e81ec56fa20
Bryan <bryanbuitrago86@gmail.com>
2018-01-30 22:02:14 +0000

    Java tutorial Classes & OOP & Constructors

---
+++ javaTut7DB/src/javaTut7DB/Monster.java
@@ -0,0 +1,70 @@
+package javaTut7DB;
+
+public class Monster {
+	
+	public final String TOMBSTONE = "Here Lies a Dead monster";
+	
+	private int health = 500;
+	private int attack = 20;
+	private int movement = 2;
+	private int xPosition = 0;
+	private int yPosition = 0;
+	private boolean alive = true;
+	
+	public String name = "Big Monster";
+	
+	public int getAttack()
+	{
+		return attack;
+	}
+	
+	public int getMovement()
+	{
+		return movement;
+	}
+	
+	public int getHealth() 
+	{
+		return health;
+	}
+	
+	public void setHealth(int decreaseHealth)
+	{
+		health = health - decreaseHealth;
+		if(health < 0)
+		{
+			alive = false;
+		}
+	}
+	
+	public void setHealth(double decrease)
+	{
+		int intDecreaseHealth = (int) decreaseHealth;
+		health = health - intDecreaseHealth;
+		if(health < 0)
+		{
+			alive = false;
+		}
+	}
+	
+	public Monster(int health, int attack, int movement)
+	{
+		this.health = health;
+		this.attack = attack;
+		this.movement = movement;
+	}
+	
+	//Default Constructor	
+	public Monster()
+	{
+		
+	}
+		
+	public static void main(String[] args)
+	{
+		Monster Frank = new Monster();
+		
+		System.out.println(Frank.attack);
+	}
+	
+}
